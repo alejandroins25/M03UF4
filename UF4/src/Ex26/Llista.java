538c34cacb68fa3a75fcd8a6f5582157e9f48389
Alejandro-PC <Alejandro-PC@AMC-PC>
2023-10-23 20:20:59 +0200

    23/10/2023

---
+++ UF4/src/Ex26/Llista.java
@@ -0,0 +1,21 @@
+package Ex26;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Llista {
+	public static void main(String[] args) {
+		String[] ingredientesPl = { "Patata", "Cebolla", "Huevo" };
+		Double[] preuIngPl = { 3.5, 2.80, 2.10 };
+		Boolean[] organicoPl = { true, true, true };
+
+		String[] ingredientesPo = { "Arroz", "Leche", "Canela" };
+		Double[] preuIngPo = { 2, 1.40, 0.50 };
+		Boolean[] organicoPo = { false, true, false };
+
+		imprimirLlista("Primer plato", "Tortilla de patata", 8.40, ingredientesPl, preuIngPl, organicoPl);
+	}
+	
+	public static void imprimirLlistat(String titol, String nomPlat, double preu, String[] ingredientesPl, double[] preuIngPl, boolean[] organicoPl) {
+		Syst
+	}
+}
